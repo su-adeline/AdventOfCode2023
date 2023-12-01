f9ed2d5e053d0edaebc0bde0889f67d7c8871d5e
su-adeline <adeline.susset@gmail.com>
2023-12-01 12:43:19 +0100

    Initial Commit

---
+++ AdventOfCode2023/AdventOfCode2023App.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCode2023App.swift
+//  AdventOfCode2023
+//
+//  Created by Adeline Susset on 01/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCode2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
